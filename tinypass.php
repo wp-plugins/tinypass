59eb604280cd715cd6c4fc339920266d26848ea0
wordpress@tinypass.com <wordpress@tinypass.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-15 15:39:54 +0000

    0.4 version
    git-svn-id: https://plugins.svn.wordpress.org/tinypass/trunk@385109 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tinypass.php
+++ tinypass.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package TinyPass
- * @version 0.1
+ * @version 0.4
  */
 /*
 Plugin Name: TinyPass
 Plugin URI: http://www.tinypass.com
 Description: TinyPass plugin for wordpress
 Author: TinyPass
-Version: 0.1
+Version: 0.4
 Author URI: http://www.tinypass.com
 */
 
