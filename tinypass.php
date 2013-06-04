6464dfb81658bfb45f9966c4e6085d6022511750
wordpress@tinypass.com <wordpress@tinypass.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-04 19:27:49 +0000

    comment changes
    git-svn-id: https://plugins.svn.wordpress.org/tinypass/branches/3.0@722735 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tinypass.php
+++ tinypass.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name: TinyPass
   Plugin URI: http://www.tinypass.com
-  Description: Tinypass is the best way to charge for access to content on your WordPress site.  To get started: 1) Click the "Activate" link to the left of this description, 2) Go to http://developer.tinypass.com/main/wordpress and follow the installation instructions to create a free Tinypass publisher account and configure the Tinypass plugin for your WordPress site
+  Description: Tinypass is the best way to charge for access to content on your WordPress site.  1) Go to http://developer.tinypass.com/main/wordpress and follow the installation instructions to create a Tinypass publisher account and configure the Tinypass plugin for your WordPress site
   Author: Tinypass
   Version: 3.0.0
   Author URI: http://www.tinypass.com
@@ -11,7 +11,7 @@
 
 $current = get_option('tinypass_version');
 
-if ($current < '3.0.0' || get_option('tinypass_legacy') == 1) {
+if (1==1 && $current < '3.0.0' || get_option('tinypass_legacy') == 1) {
 	include_once dirname(__FILE__) . '/legacy/legacy.php';
 } else {
 	include_once dirname(__FILE__) . '/jslite/tinypass.php';
